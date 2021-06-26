ea49638e67609e3a16bda5585188c572695c7dea
OsaPL <osa12pl@gmail.com>
2021-06-26 13:57:59 +0200

    woops missing file

---
+++ UserControls/SelectListMsgBox/SelectListMsgBox.axaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ClassJsonEditor.UserControls
+{
+    public class SelectListMsgBox : Window
+    {
+        public SelectListMsgBox()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private TaskCompletionSource<object> _tcs;
+
+        public Task<object> ShowListBox(Window parent, IEnumerable<object> list)
+        {
+            DataContext = list;
+            _tcs = new TaskCompletionSource<object>();
+            if (parent != null)
+                ShowDialog(parent);
+            else Show();
+            return _tcs.Task;
+        }
+
+        private async void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            _tcs.TrySetResult(e.AddedItems[0]);
+            await _tcs.Task;
+            Close();
+        }
+    }
+}
